916137e5940c81c421625d4dfdb1ac3c76ae6f9f
dnaiscienne <dnaiscienne@yahoo.com>
2015-10-20 23:17:18 +0800

    Minor tweak to the Content Provider. Also Glide.

--- Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/api/BookListAdapter.java
+++ Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/api/BookListAdapter.java
@@ -10,9 +10,10 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import it.jaschke.alexandria.R;
 import it.jaschke.alexandria.data.AlexandriaContract;
-import it.jaschke.alexandria.services.DownloadImage;
 
 /**
  * Created by saj on 11/01/15.
@@ -42,8 +43,16 @@ public class BookListAdapter extends CursorAdapter {
         ViewHolder viewHolder = (ViewHolder) view.getTag();
 
         String imgUrl = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.IMAGE_URL));
-        new DownloadImage(viewHolder.bookCover).execute(imgUrl);
-
+        /**
+        * Replaced AsyncTask with Glide. Should get rid of the MalformedURLException
+        * */
+        //        new DownloadImage(viewHolder.bookCover).execute(imgUrl);
+        // TODO: default image in case of failure
+        Glide.with(context)
+                .load(imgUrl)
+//                .error()
+                .crossFade()
+                .into(viewHolder.bookCover);
         String bookTitle = cursor.getString(cursor.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         viewHolder.bookTitle.setText(bookTitle);
 
--- Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/data/DbHelper.java
+++ Alexandria_Code/alexandria/app/src/main/java/it/jaschke/alexandria/data/DbHelper.java
@@ -14,6 +14,7 @@ public class DbHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "alexandria.db";
 
     public DbHelper(Context context) {
+
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
